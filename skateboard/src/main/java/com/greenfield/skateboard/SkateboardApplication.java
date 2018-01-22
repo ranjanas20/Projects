cc7883a110e3faac6b27427c21a0d00035313b25
sanjay <sanjay@RannoPC>
2018-01-22 12:49:20 -0800

    Initial Commit

---
+++ skateboard/src/main/java/com/greenfield/skateboard/SkateboardApplication.java
@@ -0,0 +1,12 @@
+package com.greenfield.skateboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkateboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkateboardApplication.class, args);
+	}
+}
