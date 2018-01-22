3d270426c19d01f4d0a327cbf4b130763394af95
sanjay <sanjay@RannoPC>
2018-01-22 13:10:03 -0800

    Addes New Class

---
+++ skateboard/src/main/java/com/greenfield/skateboard/Users.java
@@ -0,0 +1,10 @@
+package com.greenfield.skateboard;
+
+public class Users {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
