8d28db0c0df36868741a3921ea6db350814bcae3
Ranjana <ranjanas20@gmail.com>
2018-02-06 22:16:23 -0800

    added test case

---
+++ skateboard/src/test/java/com/greenfield/skateboard/mvc/controller/ItemControllerSpringTest.java
@@ -0,0 +1,68 @@
+package com.greenfield.skateboard.mvc.controller;
+
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.greenfield.skateboard.SkateboardApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SkateboardApplication.class)
+@WebAppConfiguration
+public class ItemControllerSpringTest {
+	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+
+	private MockMvc mockMvc;
+
+	private HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Autowired
+	void setConverters(HttpMessageConverter<?>[] converters) {
+
+		this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
+				.filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter).findAny().orElse(null);
+
+		assertNotNull("the JSON message converter must not be null", this.mappingJackson2HttpMessageConverter);
+	}
+
+	@Before
+	public void setup() throws Exception {
+		this.mockMvc = webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void findItems() throws Exception {
+		mockMvc.perform(get("/items")).andExpect(status().isOk())
+				.andExpect(content().contentType(contentType));
+	}
+
+	protected String json(Object o) throws IOException {
+		MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+		this.mappingJackson2HttpMessageConverter.write(o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+		return mockHttpOutputMessage.getBodyAsString();
+	}
+}
